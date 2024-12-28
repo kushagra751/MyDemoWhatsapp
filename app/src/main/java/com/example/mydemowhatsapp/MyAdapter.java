bddd860c3b0fe0573631db1a215f048156924ba2
Kushagra <patidarkush751@gmail.com>
2024-12-28 15:17:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mydemowhatsapp/MyAdapter.java
@@ -0,0 +1,31 @@
+package com.example.mydemowhatsapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class MyAdapter extends FragmentStateAdapter {
+    private static final int ntabs = 3;
+
+    public MyAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 1:return new Chats();
+            case 2:return new Updates();
+            case 3:return new Calls();
+            default:return new Chats();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return ntabs;
+    }
+}
